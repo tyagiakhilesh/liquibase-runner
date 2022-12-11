142e7880518030c2b245f9e9896587bfcc82fcc5
Akhilesh Tyagi <tyagiakhilesh@gmail.com>
2022-12-11 17:37:20 +0530

    Update LiquibaseRunner.java

--- src/main/java/com/mf/liquibase/LiquibaseRunner.java
+++ src/main/java/com/mf/liquibase/LiquibaseRunner.java
@@ -6,17 +6,20 @@ import liquibase.Liquibase;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.Optional;
 
 public class LiquibaseRunner {
-    private static Logger LOGGER = LoggerFactory.getLogger(LiquibaseRunner.class);
+    private static Logger logger = LoggerFactory.getLogger(LiquibaseRunner.class);
 
     private DataSource dataSource;
 
@@ -24,19 +27,21 @@ public class LiquibaseRunner {
         this.dataSource = dataSource;
     }
 
-    public void runChangeLog(final String changelog, final Optional<Map<String, Object>> changeLogProperties) {
-        try(Connection connection = dataSource.getConnection()) {
+    public void runChangeLog(final String changelog, final Optional<Map<String, Object>> properties)
+            throws SQLException, LiquibaseException {
+        try (final Connection connection = dataSource.getConnection()) {
             final DatabaseFactory databaseFactory = DatabaseFactory.getInstance();
             final Database database = databaseFactory.findCorrectDatabaseImplementation(new JdbcConnection(connection));
             final Liquibase liquibase = new Liquibase(changelog, new ClassLoaderResourceAccessor(), database);
-            if (changeLogProperties.isPresent()) {
-                changeLogProperties.get().entrySet().stream().forEach(entry -> {
-                    liquibase.setChangeLogParameter(entry.getKey(), entry.getValue());
-                });
-            }
+            properties.ifPresent(stringObjectMap -> stringObjectMap.entrySet().stream()
+                    .forEach(entry -> liquibase.setChangeLogParameter(entry.getKey(), entry.getValue())));
             liquibase.update(new Contexts(""), new LabelExpression(""));
-        } catch (final Exception ex) {
-            LOGGER.error("Running changelogs.", ex);
+        } catch (SQLException | DatabaseException throwables) {
+            logger.error("Error running changelog.", throwables);
+            throw throwables;
+        } catch (LiquibaseException e) {
+            logger.error("Error running changelog.", e);
+            throw e;
         }
     }
 }
